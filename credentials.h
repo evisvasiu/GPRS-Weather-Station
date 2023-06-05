86d82cb90c274649b3aae0458969c7bef2ec3dc3
Evis Vasiu <evis.vasiu@gmail.com>
2023-06-05 23:21:35 +0200

    credentials in a seperate file

---
+++ credentials.h
@@ -0,0 +1,11 @@
+// Your GPRS credentials, if any
+const char apn[] = "vodafoneweb";
+const char gprsUser[] = "";
+const char gprsPass[] = "";
+
+// MQTT details
+const char *broker = "138.3.246.220";
+const char *mqtt_user = "jezerca";
+const char *mqtt_pass = "Password@2";
+
+const char *topicInit = "GsmClientTest/init";
